650adfdf67a6b2dc69fd500e8bc13343986b3d59
Kudyakov Artem <t3m8ch@yandex.ru>
2024-11-22 19:09:21 +0400

    feat: Create DB for backend

--- db/CreateTables.sql
+++ db/CreateTables.sql
@@ -10,3 +10,109 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash BYTEA NOT NULL,
     password_salt BYTEA NOT NULL
 );
+
+CREATE TABLE sports (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE teams (
+    name TEXT NOT NULL,
+    sport_name TEXT NOT NULL,
+    PRIMARY KEY (name, sport_name),
+    FOREIGN KEY (sport_name) REFERENCES sports(name)
+);
+
+CREATE TABLE gender_and_age_groups (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE programs_disciplines (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE countries (
+    country TEXT PRIMARY KEY
+);
+
+CREATE TABLE regions (
+    region TEXT PRIMARY KEY,
+    country TEXT NOT NULL,
+    FOREIGN KEY (country) REFERENCES countries(country)
+);
+
+CREATE TABLE cities (
+    city TEXT PRIMARY KEY,
+    region TEXT,
+    country TEXT NOT NULL,
+    FOREIGN KEY (region) REFERENCES regions(region),
+    FOREIGN KEY (country) REFERENCES countries(country)
+);
+
+CREATE TABLE competitions (
+    ekp_id BIGINT PRIMARY KEY,
+    members_count INT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+
+    country TEXT NOT NULL,
+    region TEXT,
+    city TEXT NOT NULL,
+    sport_name TEXT NOT NULL,
+    team_name TEXT NOT NULL,
+
+    FOREIGN KEY (country) REFERENCES countries(country),
+    FOREIGN KEY (region) REFERENCES regions(region),
+    FOREIGN KEY (city) REFERENCES cities(city),
+    FOREIGN KEY (sport_name) REFERENCES sports(name),
+    FOREIGN KEY (sport_name, team_name) REFERENCES teams(sport_name, name)
+);
+
+CREATE TABLE competitions_programs_discipline (
+    program_discipline_name TEXT NOT NULL,
+    ekp_id BIGINT NOT NULL,
+
+    PRIMARY KEY (program_discipline_name, ekp_id),
+    FOREIGN KEY (program_discipline_name) REFERENCES programs_disciplines(name),
+    FOREIGN KEY (ekp_id) REFERENCES competitions(ekp_id)
+);
+
+CREATE TABLE competitions_gender_and_age_groups (
+    gender_and_age_group_name TEXT NOT NULL,
+    ekp_id BIGINT NOT NULL,
+
+    PRIMARY KEY (gender_and_age_group_name, ekp_id),
+    FOREIGN KEY (gender_and_age_group_name) REFERENCES gender_and_age_groups(name),
+    FOREIGN KEY (ekp_id) REFERENCES competitions(ekp_id)
+);
+
+CREATE TABLE competition_types (
+    sport_name TEXT NOT NULL,
+    team_name TEXT NOT NULL,
+    name TEXT NOT NULL,
+
+    PRIMARY KEY (name, sport_name, team_name),
+    FOREIGN KEY (sport_name) REFERENCES sports(name),
+    FOREIGN KEY (sport_name, team_name) REFERENCES teams(sport_name, name)
+);
+
+CREATE TABLE teams_gender_and_age_groups (
+    gender_and_age_group_name TEXT NOT NULL,
+    sport_name TEXT NOT NULL,
+    team_name TEXT NOT NULL,
+
+    PRIMARY KEY (gender_and_age_group_name, sport_name, team_name),
+    FOREIGN KEY (gender_and_age_group_name) REFERENCES gender_and_age_groups(name),
+    FOREIGN KEY (sport_name) REFERENCES sports(name),
+    FOREIGN KEY (sport_name, team_name) REFERENCES teams(sport_name, name)
+);
+
+CREATE TABLE teams_programs_disciplines (
+    program_discipline_name TEXT NOT NULL,
+    sport_name TEXT NOT NULL,
+    team_name TEXT NOT NULL,
+
+    PRIMARY KEY (program_discipline_name, sport_name, team_name),
+    FOREIGN KEY (program_discipline_name) REFERENCES programs_disciplines(name),
+    FOREIGN KEY (sport_name) REFERENCES sports(name),
+    FOREIGN KEY (sport_name, team_name) REFERENCES teams(sport_name, name)
+);
--- db/DropTables.sql
+++ db/DropTables.sql
@@ -1,2 +1,16 @@
 DROP TABLE IF EXISTS users;
 DROP TYPE IF EXISTS user_role;
+
+DROP TABLE IF EXISTS teams_programs_disciplines;
+DROP TABLE IF EXISTS teams_gender_and_age_groups;
+DROP TABLE IF EXISTS competition_types;
+DROP TABLE IF EXISTS competitions_gender_and_age_groups;
+DROP TABLE IF EXISTS competitions_programs_discipline;
+DROP TABLE IF EXISTS competitions;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS regions;
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS programs_disciplines;
+DROP TABLE IF EXISTS gender_and_age_groups;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS sports;
