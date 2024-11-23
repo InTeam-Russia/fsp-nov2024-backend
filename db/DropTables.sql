de4d1a03b9b7109ebcc4a6c406ab6688a032d551
Kudyakov Artem <t3m8ch@yandex.ru>
2024-11-23 15:45:27 +0400

    fix: DropTables.sql

--- db/DropTables.sql
+++ db/DropTables.sql
@@ -3,10 +3,10 @@ DROP TYPE IF EXISTS user_role;
 
 DROP TABLE IF EXISTS teams_programs_disciplines;
 DROP TABLE IF EXISTS teams_gender_and_age_groups;
-DROP TABLE IF EXISTS competition_types;
 DROP TABLE IF EXISTS competitions_gender_and_age_groups;
 DROP TABLE IF EXISTS competitions_programs_discipline;
 DROP TABLE IF EXISTS competitions;
+DROP TABLE IF EXISTS competition_types;
 DROP TABLE IF EXISTS cities;
 DROP TABLE IF EXISTS regions;
 DROP TABLE IF EXISTS countries;
